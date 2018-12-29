1909180f2950f47ca1870e0e4d67ae5ede293918
Abhinav Pappu <abhi1pappu9@gmail.com>
2018-12-29 02:18:32 -0600

    Add animations for register button and menu

--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
 import './style.sass';
 
+const $menu = document.querySelector('.menu');
+
 setTimeout(() => {
   document.getElementById('top').classList.add('shift-up');
-}, 250);
+}, 300);
 
-// document.addEventListener("DOMContentLoaded", () => {
-//   document.getElementById('top').classList.add('shift-up');
-// })
+
+window.addEventListener('scroll', setMenuStickiness);
+
+function setMenuStickiness() {
+  const menuHeight = $menu.offsetHeight;
+
+  if (window.scrollY < (window.innerHeight - menuHeight)) { // menu should be in normal position
+    $menu.classList.remove('sticky');
+  } else { // menu should be sticky
+    $menu.classList.add('sticky');
+  }
+}
