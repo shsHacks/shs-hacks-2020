b3c2024adfe60fa2c1d327a24041616a52cf1426
Abhinav Pappu <abhi1pappu9@gmail.com>
2019-01-12 19:29:14 -0600

    Make about section more readable

--- src/sections/About.vue
+++ src/sections/About.vue
@@ -1,7 +1,8 @@
 <template>
   <sliding-title title="About Us" :scroll="scroll" :color="color">
     <div class="text">
-      <p>shsHacks is Stevenson High School’s first ever Hackathon, a collaborative, <span class="highlight">24-hour</span> coding marathon that will take place March 2-3. Get ready to make the best game, app, or website of your life so far in just one weekend. Never coded before? Just started? New to hands-on project development? No problem! shsHacks is any and all students who are interested in inventing, designing, and bringing their creations to life, from beginners to seasoned developers.</p>
+      <p>shsHacks is Stevenson High School’s first ever Hackathon, a collaborative, <span class="highlight">24-hour</span> coding marathon that will take place March 2-3. Get ready to make the best game, app, or website of your life so far in just one weekend.</p>
+      <p>Never coded before? Just started? New to hands-on project development? No problem! shsHacks is for any and all students who are interested in inventing, designing, and bringing their creations to life, from beginners to seasoned developers.</p>
       <p>shsHacks is student-run, student-organized, and completely <span class="highlight">free</span> for all participants. For more info, check out the FAQs or contact us below.</p>
     </div>
   </sliding-title>
