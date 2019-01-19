c149987a67cf23ab4c454a4aebf7050fec261a61
Abhinav Pappu <abhi1pappu9@gmail.com>
2019-01-18 23:54:59 -0600

    Add Sponsors section and Wolfram as a sponsor

--- src/sections/Sponsors.vue
+++ src/sections/Sponsors.vue
@@ -1,14 +1,60 @@
 <template>
-  <div></div>
+  <div>
+    <p class="title">A Huge Thank You to Our Generous Sponsors!</p>
+    <div class="sponsors">
+      <a v-for="sponsor in sponsors" class="sponsor" :href="sponsor.link" :key="sponsor.image">
+        <img :src="`/assets/sponsors/${sponsor.image}`">
+      </a>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+let sponsors = [
+  { image: 'wolfram.png', link: 'http://www.wolfram.com/' },
+]
 
+export default {
+  data() {
+    return {
+      sponsors,
+    }
+  }
 }
 </script>
 
 <style lang="sass" scoped>
+@import 'src/style.sass'
+
+.title
+  text-align: center
+  margin: 75px 10px 15px 10px
+  color: rgba(0, 0, 0, 0.8)
+  font-size: 2em
+  font-weight: bold
+  +mobile-and-tablet
+    margin-top: 50px
+  +mobile
+    margin-left: 5px
+    margin-right: 5px
+    font-size: 1.7em
+
+.sponsors
+  max-width: 1000px
+  margin: auto
+  display: flex
+  flex-flow: row wrap
+  justify-content: space-around
+
+  .sponsor
+    width: 25%
+    margin: 25px
+    +mobile
+      width: 40%
+      margin: 15px
+
+    img
+      width: 100%
 
 </style>
 
