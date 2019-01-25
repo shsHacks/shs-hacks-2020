b5f2718c8892ff3c03cf8f622d1a379092a3c4ff
Abhinav Pappu <abhi1pappu9@gmail.com>
2019-01-24 19:18:37 -0600

    Track whether user selects 'Yes' or 'No' for is Stevenson student

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue';
 import { VueHammer } from 'vue2-hammer';
+import VueAnalytics from "vue-analytics";
 import App from 'src/App.vue';
 
 import smoothscroll from "smoothscroll-polyfill";
 
 smoothscroll.polyfill();
+
+Vue.use(VueAnalytics, {
+  id: 'UA-132318363-1',
+});
 
 Vue.use(VueHammer);
 new Vue({
